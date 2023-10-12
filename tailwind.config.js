fb08099aaebd1e43aa1ad36baec32e2e72a9cf77
Mohanraj7263 <mohanraj.maduraimuthu@flyerssoft.com>
2023-10-12 18:16:40 +0530

    Tailwind Finished

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        opensans: ["Open Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
